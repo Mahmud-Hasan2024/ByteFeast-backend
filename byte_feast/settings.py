7f3a840623f5d98c4a5e80924502d74dfb68d1b4
Mahmud Hasan <mh.kid234@gmail.com>
2025-11-03 16:32:49 +0600

    corrected frontend domain and url from .env

--- byte_feast/settings.py
+++ byte_feast/settings.py
@@ -95,7 +95,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://byte-feast-react-w8qi.vercel.app",
-    "https://byte-feast-react.vercel.app",
 ]
 
 
